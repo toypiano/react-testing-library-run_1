22a8b8e260cb84889f2509e84b93bc6ec5caad52
Hayoun Lee <sidhlee@gmail.com>
2020-02-24 17:54:27 +0900

    Add assertions

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -3,7 +3,12 @@ import { FavoriteNumber } from "../favorite-number";
 import ReactDOM from "react-dom";
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement("div"); // create a DOM element
-  ReactDOM.render(<FavoriteNumber />, div); // Render ReactElement inside div
+  const div = document.createElement("div"); // create a container DOM element
+  ReactDOM.render(<FavoriteNumber />, div); // Render ReactElement inside container
   console.log(div.innerHTML); // print component
+  // query around container and expect
+  expect(div.querySelector("input").type).toBe("number");
+  expect(div.querySelector("label").textContent).toBe(
+    "Favorite Number"
+  );
 });
