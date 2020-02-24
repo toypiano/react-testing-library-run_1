647a131ad3cdcceb9293cae5d65eff6935b0174d
Hayoun Lee <sidhlee@gmail.com>
2020-02-24 19:03:47 +0900

    Import render function from testing-library/react + Remove dependencies

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,18 +1,6 @@
 import React from "react";
 import { FavoriteNumber } from "../favorite-number";
-import { getQueriesForElement } from "@testing-library/dom";
-import ReactDOM from "react-dom";
-
-// abstract away code for
-// 1. creating container
-// 2. rendering react component that you want to test inside container
-// 3. return query methods bound with that container
-function render(ui) {
-  const container = document.createElement("div");
-  ReactDOM.render(ui, container);
-  const queries = getQueriesForElement(container);
-  return { container, ...queries };
-}
+import { render } from "@testing-library/react";
 
 test('renders a number input with a label "Favorite Number"', () => {
   const { getByLabelText } = render(<FavoriteNumber />);
