693629b32014d2c6c55e142be54ac95991cd7de1
Hayoun Lee <sidhlee@gmail.com>
2020-02-24 18:47:22 +0900

    Bind element query methods with container using getQueriesForElement

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { FavoriteNumber } from "../favorite-number";
-import { queries } from "@testing-library/dom";
+import { getQueriesForElement } from "@testing-library/dom";
 import ReactDOM from "react-dom";
 
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement("div"); // create a container DOM element
   ReactDOM.render(<FavoriteNumber />, div); // Render ReactElement inside container
 
+  // return query methods bound with the container
+  const { getByLabelText } = getQueriesForElement(div);
+
   // find input element that correspond to given label text from the specified DOM node.
   // Just like the way the user identifies form control by the label it's associated with.
-  const input = queries.getByLabelText(div, /favorite number/i);
+  const input = getByLabelText(/favorite number/i);
 
   // query around container and expect
   expect(input).toHaveAttribute("type", "number");
