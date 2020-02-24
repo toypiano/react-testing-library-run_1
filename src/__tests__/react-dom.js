423910ebccafbede26860314b2867bb36f986465
Hayoun Lee <sidhlee@gmail.com>
2020-02-24 18:12:10 +0900

    Import all matchers from jest-dom and extend them

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,13 +1,10 @@
 // add custom jest matchers to test the state of DOM
-import {
-  toHaveAttribute,
-  toHaveTextContent
-} from "@testing-library/jest-dom";
+import * as jestDOM from "@testing-library/jest-dom";
 import React from "react";
 import { FavoriteNumber } from "../favorite-number";
 import ReactDOM from "react-dom";
 
-expect.extend({ toHaveAttribute, toHaveTextContent }); // provides more useful
+expect.extend(jestDOM); // provides more useful
 
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement("div"); // create a container DOM element
